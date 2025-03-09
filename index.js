bd1a2cdfcd966fa1ebecd778c5cdbc30af47d6f2
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-09 19:04:08 +0300

    makes modifications to the loops in index.js

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
 
 function writeCards(names, event) {
-    const messages = [];
+  let messages = [];
 
-    for (let i = 0; i <names.length; i++) {
-        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-        messages.push(message);
-    }
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    debugger;
+  }
 
-    return messages;
+  return messages;
 }
 
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+writeCards(names, event);
 
-
+let number = "100"
 function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number--;
-    }
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
 }
+
+countDown(number);
+
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
